669488ffa0a209754be46548756869282133c8d9
Arsam Amini <digitgraming@gmail.com>
2023-07-27 12:02:36 +0330

    Set a password

--- gitmit3.py
+++ gitmit3.py
@@ -1,5 +1,9 @@
-a=int(input("enter your number:"))
-if a%2==0:
-    print("even")
+pas=int(input("What's your password? "))
+if pas==4216:
+    a=int(input("enter your number:"))
+    if a%2==0:
+        print("even")
+    else:
+        print("odd")
 else:
-    print("odd")
+    print("Invalid password!")
